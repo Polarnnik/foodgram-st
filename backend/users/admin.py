5fc6127eceaaedacd5f89e83aed0b3d54227e0ed
Polarnnik <rostislav.voloshin@yandex.ru>
2025-06-12 22:54:34 +0700

    Добавленнп пдминка для моделей Usser и Follow

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import Follow
+
+User = get_user_model()
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    ordering = ['email']
+    list_display = (
+        'id', 'email', 'username', 'first_name', 'last_name', 'is_staff', )
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('email', 'username', 'first_name', 'last_name',)
+    readonly_fields = ('last_login', 'date_joined')
+
+@admin.register(Follow)
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('user', 'author')
+    search_fields = ('user__username', 'author__username')
+    list_filter = ('author',)
