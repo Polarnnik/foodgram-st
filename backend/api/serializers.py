7c65af456778a74420ed46816c06dc986693fdcf
Polarnnik <rostislav.voloshin@yandex.ru>
2025-06-14 00:33:13 +0700

    Реализовать создание/обновление рецептов с: - Проверкой уникальных ингредиентов/тегов - Обязательной загрузкой изображения - Корректной обработкой частичных обновлений
    Зафиксированны пакеты

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,8 @@
-from recipes.models import Tag, Ingredient, RecipeIngredient, Recipe
+from recipes.models import Tag, Ingredient, RecipeIngredient, Recipe,  Favorite, ShoppingCart
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from drf_extra_fields.fields import Base64ImageField
+from django.db import transaction
 
 User = get_user_model()
 
@@ -80,6 +81,90 @@ class RecipeIngredientWriteSerializer(serializers.ModelSerializer):
     class Meta:
         model = RecipeIngredient
         fields = ('id', 'amount')
+
+class RecipeWriteSerializer(serializers.ModelSerializer):
+    ingredients = RecipeIngredientWriteSerializer(many=True, allow_empty=False)
+    tags = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.all(),
+        many=True,
+        allow_empty=False,
+        write_only=True,
+        required=False
+    )
+    image = Base64ImageField()
+
+    class Meta:
+        model = Recipe
+        fields = ('ingredients', 'tags', 'image', 'name', 'text', 'cooking_time')
+
+    def validate_ingredients(self, value):
+        ingredient_ids = [item['ingredient'].id for item in value]
+        if len(ingredient_ids) != len(set(ingredient_ids)):
+            raise serializers.ValidationError('Ingredients must be unique.')
+        return value
+
+    def validate_tags(self, value):
+        if len(value) != len(set(value)):
+            raise serializers.ValidationError('Tags must be unique.')
+        return value
+
+    def validate_image(self, value):
+        if value in (None, ''):
+            raise serializers.ValidationError('Image field may not be empty.')
+        return value
+
+    def create_ingredients(self, recipe, ingredients_data):
+        RecipeIngredient.objects.bulk_create([
+            RecipeIngredient(
+                recipe=recipe,
+                ingredient=item['ingredient'],
+                amount=item['amount']
+            ) for item in ingredients_data
+        ])
+
+    @transaction.atomic
+    def create(self, validated_data):
+        ingredients = validated_data.pop('ingredients')
+        tags = validated_data.pop('tags', []) # <--- Безопасно извлекаем. Если тегов нет, будет []
+        user = self.context['request'].user
+        recipe = Recipe.objects.create(author=user, **validated_data)
+        recipe.tags.set(tags)
+        recipe.is_favorited = False
+        recipe.is_in_shopping_cart = False
+        self.create_ingredients(recipe, ingredients)
+        return recipe
+
+    @transaction.atomic
+    def update(self, instance, validated_data):
+        request = self.context.get('request')
+        is_partial = request and request.method == 'PATCH'
+
+        if is_partial and 'ingredients' not in self.initial_data:
+            raise serializers.ValidationError({
+                'ingredients': 'This field is required.'
+            })
+
+        tags = validated_data.pop('tags', None)
+        ingredients = validated_data.pop('ingredients', None)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        if tags is not None:
+            instance.tags.set(tags)
+        if ingredients is not None:
+            RecipeIngredient.objects.filter(recipe=instance).delete()
+            self.create_ingredients(instance, ingredients)
+
+        instance.save()
+
+        user = request.user
+        instance.is_favorited = Favorite.objects.filter(user=user, recipe=instance).exists()
+        instance.is_in_shopping_cart = ShoppingCart.objects.filter(user=user, recipe=instance).exists()
+        return instance
+
+    def to_representation(self, instance):
+        return RecipeReadSerializer(instance, context=self.context).data
 
 class FollowSerializer(UserSerializer):
     recipes = serializers.SerializerMethodField()
