3373c719f5e53e116dc0ed7a02e6faf8e6cc595b
Polarnnik <rostislav.voloshin@yandex.ru>
2025-06-14 01:41:24 +0700

    Изменнено: - Настроенны эндпоинты - Написанны базовые вьюсеты - Добавленны разрешения и пагинация

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from .serializers import (AvatarSerializer, FollowSerializer,
+                          IngredientSerializer, RecipeMiniSerializer,
+                          RecipeReadSerializer, RecipeWriteSerializer,
+                          TagSerializer, UserSerializer)
+from recipes.models import Favorite, Ingredient, Recipe, ShoppingCart, Tag, RecipeIngredient
+from users.models import Follow, User
+from rest_framework import status, viewsets
+from rest_framework.permissions import (AllowAny, IsAuthenticated,
+                                        IsAuthenticatedOrReadOnly)
+from django_filters.rest_framework import DjangoFilterBackend
+from .filters import IngredientFilter, RecipeFilter
+from .pagination import StandardResultsSetPagination
+from .permissions import IsCreatorOrReadOnly
+from djoser.views import UserViewSet as DjoserUserViewSet
+from rest_framework.response import Response
+from rest_framework.decorators import action
 
-# Create your views here.
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = [AllowAny]
+    pagination_class = None
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    permission_classes = [AllowAny]
+    pagination_class = None
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = IngredientFilter
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly, IsCreatorOrReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = RecipeFilter
+    pagination_class = StandardResultsSetPagination
+
+    def get_queryset(self):
+        return Recipe.objects.with_user_annotations(self.request.user).order_by('-pub_date')
+
+    def get_serializer_class(self):
+        if self.action in ('list', 'retrieve'):
+            return RecipeReadSerializer
+        return RecipeWriteSerializer
+
+class UserViewSet(DjoserUserViewSet):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    pagination_class = StandardResultsSetPagination
+
+    @action(
+        detail=False,
+        methods=['get'],
+        permission_classes=[IsAuthenticated],
+        serializer_class=UserSerializer,
+        pagination_class=None
+    )
+    def me(self, request, *args, **kwargs):
+        serializer = self.get_serializer(request.user)
+        return Response(serializer.data)
