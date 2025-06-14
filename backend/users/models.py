730a20955c927d559a0d8ee5b4ddd8687ceed02a
Polarnnik <rostislav.voloshin@yandex.ru>
2025-06-14 16:27:45 +0700

    Финальные настройки и валидация имени пользователя

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.core.validators import RegexValidator
 
 
 class User(AbstractUser):
@@ -13,6 +14,12 @@ class User(AbstractUser):
         max_length=150,
         unique=True,
         verbose_name='Username',
+        validators=[
+                RegexValidator(
+                    regex=r'^[\w.@+-]+$',
+                    message='Недопустимое имя пользователя.'
+                )
+            ]
     )
     first_name = models.CharField(
         max_length=150,
