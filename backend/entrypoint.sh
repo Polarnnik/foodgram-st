b4851bb771f7cd653995b14e50693c223cb231a3
Polarnnik <rostislav.voloshin@yandex.ru>
2025-06-18 11:45:12 +0700

    Импорт файла с ингридиентами в докере

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -11,6 +11,8 @@ echo "PostgreSQL started"
 echo "Running database migrations..."
 python manage.py migrate
 
+python manage.py import_ingridients
+
 echo "Collecting static files..."
 python manage.py collectstatic --no-input
 
